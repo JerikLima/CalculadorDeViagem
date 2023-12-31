daec28a6082ba3fd4e4a5bbb6e164b953fa1eda9
Jeriklima <jonherik123@gmail.com>
2023-12-30 23:41:44 -0300

    Logica do calculo aplicada

--- app/src/main/java/com/jeriklima/travelcalculator/MainActivity.kt
+++ app/src/main/java/com/jeriklima/travelcalculator/MainActivity.kt
@@ -2,10 +2,35 @@ package com.jeriklima.travelcalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast.*
+import com.jeriklima.travelcalculator.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.buttonCalculate.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        if (view.id == R.id.button_Calculate) {
+            calculate()
+        }
+    }
+
+    private fun calculate() {
+        val distance = binding.editDistance.text.toString().toFloat()
+        val price = binding.editPrice.text.toString().toFloat()
+        val autonomy = binding.editAutonomy.text.toString().toFloat()
+
+        val totalValue = (price*distance)/autonomy
+
+        binding.textRes.text = "R$ ${"%.2f".format(totalValue)}"
+
+        makeText(this, binding.textRes.text, LENGTH_SHORT).show()
+
     }
 }
